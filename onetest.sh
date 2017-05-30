b675967a3cd3c246e03be3ba5cc8088a9a612be1
Michael West <quagly@gmail.com>
2017-05-30 10:10:20 -0700

    added comments to explain the purpose of onetest.sh

--- onetest.sh
+++ onetest.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# run just one test
+# useful when focusing on one test issue and just want a quick single test
+# change the name of test.single to whatever test name you wish to run
+
 ./gradlew -Dtest.single=ServerBasic test
